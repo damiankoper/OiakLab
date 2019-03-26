c29a83849d1ec77f83ae10f87ef5fa84e881058e
Damian Koper <kopernickk@gmail.com>
2019-03-26 22:58:04 +0100

    Yeey works

--- lab_2/showEnv.s
+++ lab_2/showEnv.s
@@ -1,12 +1,49 @@
 .data
-
+  str1: .string "Parametry wywołania:"
+  str2: .string "Zmienne środowiskowe:"
+  spc: .ascii " "
 .text
 .globl _start
 
-exit:
-  mov $1, %eax
-  mov $0, %ebx
-  int $0x80
+# Podłączam część skryptów pomocniczych
+.include "utils/exit.s"
+.include "utils/printStr.s"
 
-_start:
+
+_start:  
+  lea str1, %eax
+  push %eax
+  call printStr
+  add $4, %esp # pop bez zapisywania
+
+  # printuj argumenty
+  mov (%esp), %eax
+  xor %ecx, %ecx
+  arguments:
+    push 4(%esp, %ecx, 4)
+    call printStr
+    add $4, %esp # pop bez zapisywania
+    inc %ecx
+    cmp %eax, %ecx
+    jne arguments
+
+  lea str2, %eax
+  push %eax
+  call printStr
+  add $4, %esp # pop bez zapisywania
+
+  # printuj argumenty
+  mov (%esp), %eax
+  inc %eax
+  env:
+    cmp $0, 4(%esp, %eax, 4)
+    je stopEnv
+    push 4(%esp, %eax, 4)
+    call printStr
+    add $4, %esp # pop bez zapisywania
+    inc %eax
+    jne env
+  stopEnv:
+
   jmp exit
+          
